d83dd32125c164e26f7542dd5ab2c19aaf61652d
jogray72 <34323688+jogray72@users.noreply.github.com>
2020-02-05 21:12:56 -0500

    Update MergeViewer.py

--- MergeViewer.py
+++ MergeViewer.py
@@ -20,6 +20,7 @@ import random
 
 import cv2
 import numpy as np
+from numpy import zeros
 
 import math
 
@@ -114,7 +115,7 @@ verticalView = math.atan(math.tan(diagonalView / 2) * (verticalAspect / diagonal
 H_FOCAL_LENGTH = image_width / (2 * math.tan((horizontalView / 2)))
 V_FOCAL_LENGTH = image_height / (2 * math.tan((verticalView / 2)))
 # blurs have to be odd
-green_blur = 1
+green_blur = 9
 orange_blur = 27
 yellow_blur = 1
 
@@ -403,66 +404,66 @@ def findBall(contours, image, centerX, centerY):
 
 
 def order_points(pts):
-	# initialize a list of coordinates that will be ordered
-	# such that the first entry in the list is the top-left,
-	# the second entry is the top-right, the third is the
-	# bottom-right, and the fourth is the bottom-left
-	rect = np.zeros((4, 2), dtype="float32")
+    # initialize a list of coordinates that will be ordered
+    # such that the first entry in the list is the top-left,
+    # the second entry is the top-right, the third is the
+    # bottom-right, and the fourth is the bottom-left
+    rect = np.zeros((4, 2), dtype="float32")
  
-	# the top-left point will have the smallest sum, whereas
-	# the bottom-right point will have the largest sum
-	s = pts.sum(axis=1)
-	rect[0] = pts[np.argmin(s)]
-	rect[3] = pts[np.argmax(s)]
+    # the top-left point will have the smallest sum, whereas
+    # the bottom-right point will have the largest sum
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[3] = pts[np.argmax(s)]
  
-	# now, compute the difference between the points, the
-	# top-right point will have the smallest difference,
-	# whereas the bottom-left will have the largest difference
-	diff = np.diff(pts, axis=1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[2] = pts[np.argmax(diff)]
+    # now, compute the difference between the points, the
+    # top-right point will have the smallest difference,
+    # whereas the bottom-left will have the largest difference
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[2] = pts[np.argmax(diff)]
  
-	# return the ordered coordinates
-	return rect
+    # return the ordered coordinates
+    return rect
 
 def four_point_transform(image, pts):
     #global warped
-	# obtain a consistent order of the points and unpack them
-	# individually
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
+    # obtain a consistent order of the points and unpack them
+    # individually
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
  
-	# compute the width of the new image, which will be the
-	# maximum distance between bottom-right and bottom-left
-	# x-coordiates or the top-right and top-left x-coordinates
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
+    # compute the width of the new image, which will be the
+    # maximum distance between bottom-right and bottom-left
+    # x-coordiates or the top-right and top-left x-coordinates
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
  
-	# compute the height of the new image, which will be the
-	# maximum distance between the top-right and bottom-right
-	# y-coordinates or the top-left and bottom-left y-coordinates
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
+    # compute the height of the new image, which will be the
+    # maximum distance between the top-right and bottom-right
+    # y-coordinates or the top-left and bottom-left y-coordinates
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
  
-	# now that we have the dimensions of the new image, construct
-	# the set of destination points to obtain a "birds eye view",
-	# (i.e. top-down view) of the image, again specifying points
-	# in the top-left, top-right, bottom-right, and bottom-left
-	# order
-	dst = np.array([
-		[0, 0],
-		[maxWidth - 1, 0],
-		[maxWidth - 1, maxHeight - 1],
-		[0, maxHeight - 1]], dtype = "float32")
+    # now that we have the dimensions of the new image, construct
+    # the set of destination points to obtain a "birds eye view",
+    # (i.e. top-down view) of the image, again specifying points
+    # in the top-left, top-right, bottom-right, and bottom-left
+    # order
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
  
-	# compute the perspective transform matrix and then apply it
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
  
-	# return the warped image
-	return warped    
+    # return the warped image
+    return warped    
 
 """ # # 3D Rotation estimation
 # def findTvecRvec(image, topLeft, topRight, bottomLeft, bottomRight):
@@ -506,6 +507,86 @@ def four_point_transform(image, pts):
 #     print ("Translation Vector:\n {0}".format(translation_vector))
 #     return rotation_vector, translation_vector """
 
+#def findBottomCorners(contour, rightmost, leftmost, bottommost)
+
+def findCorners(contour, numCorners): 
+    blackImage = zeros([480,640,1],dtype=np.uint8)
+    hull = cv2.convexHull(contour)
+    # Taking a matrix of size 5 as the kernel 
+    #kernel = np.ones((5,5), np.uint8) 
+
+    #image = cv2.drawContours(blackImage, [contour], -1, (255,255,255), cv2.FILLED)
+    #image = cv2.drawContours(blackImage, [contour], -1, (255,255,255), cv2.FILLED)
+    image = cv2.drawContours(blackImage, [hull], -1, (255,255,255), cv2.FILLED)
+
+    #cv2.fitLine()
+
+    #now use canny edge detection
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
+    cv2.imshow("edges", edges)
+
+    #minLineLength = 10
+    #maxLineGap = 5
+    #lines = cv2.HoughLinesP(edges,1,np.pi/180,100,minLineLength,maxLineGap)
+    #for x1,y1,x2,y2 in lines[0]:
+    #    cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+   
+   # lines = cv2.HoughLines(edges, 1, np.pi/180, 200) 
+   # print("lines: " + str(len(lines)))
+   
+
+    #for line in lines:
+    #    rho,theta = line[0]
+    #    a = np.cos(theta)
+    #    b = np.sin(theta)
+    #    x0 = a * rho
+    #    y0 = b * rho
+        # x1 stores the rounded off value of (r * cos(theta) - 1000 * sin(theta))
+    #    x1 = int(x0 + 1000 * (-b))
+        # y1 stores the rounded off value of (r * sin(theta)+ 1000 * cos(theta))
+    #    y1 = int(y0 + 1000 * (a))
+        # x2 stores the rounded off value of (r * cos(theta)+ 1000 * sin(theta))
+    #    x2 = int(x0 - 1000 * (-b))
+        # y2 stores the rounded off value of (r * sin(theta)- 1000 * cos(theta))
+    #    y2 = int(y0 - 1000 * (a))
+    #    cv2.line(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+    cv2.imshow('image', img)
+
+   
+
+   
+    
+    # The first parameter is the original image, 
+    # kernel is the matrix with which image is  
+    # convolved and third parameter is the number  
+    # of iterations, which will determine how much  
+    # you want to erode/dilate a given image.  
+    #img_erosion = cv2.erode(image, kernel, iterations=0) 
+    #img_dilation = cv2.dilate(img_erosion, kernel, iterations=0) 
+   
+    #print('hull contour length = ', len(hull))
+    #image = cv2.drawContours(blackImage, [img_erosion], -1, (255,255,255), cv2.FILLED)
+    #image = cv2.drawContours(blackImage, [contour], 0, (255,255,255), 1)
+
+    #cv2.dilate(image)
+
+    #gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    #corners = cv2.goodFeaturesToTrack(edges,numCorners,0.1,30)
+    #corners = cv2.goodFeaturesToTrack(lines,0,0.05,30)
+    #corners = cv2.goodFeaturesToTrack(image,numCorners,0.01,40)
+    #corners = np.int0(corners)
+    #print("corners len: " + str(len(corners)))
+
+    #outer_corners = np.array([leftmost, rightmost, bottommost], dtype="double")
+    #sorted_corners = order_points(corners)
+    
+    #for i in corners:
+    #   x,y = i.ravel()
+    #   cv2.circle(edges,(x,y),3,(125),-1)
+
+   # cv2.imshow("corners", corners)
 
 # 3D Rotation estimation
 def findTvecRvec(image, outer_corners):
@@ -621,12 +702,40 @@ def findTape(contours, image, centerX, centerY):
        
         for cnt in cntsSorted:
             x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+
+            # rotated rectangle
+            rect = cv2.minAreaRect(cnt)
+            print('rotated rectangle = ',rect)
+            (x,y),(width,height),angleofrotation = rect
+            box = cv2.boxPoints(rect)
+            box = np.int0(box)
+            cv2.drawContours(image,[box],0,(255,0,0),2)
+            print("box points: " + str(box))
+            print("rotation angle" + str(angleofrotation))
+
+            hull = cv2.convexHull(cnt)
+            #print('hull', hull)
+            print('hull contour length = ', len(hull))
+            cv2.drawContours(image, [hull], -1, (0,0,255), cv2.FILLED)
+            #cv2.imshow('hull over yellow mask', imgContours)
+            hull_area = cv2.contourArea(hull)
+            #print('solidity from convex hull', float(area)/hull_area)
 
             # extreme points
             leftmost = tuple(cnt[cnt[:,:,0].argmin()][0])
             rightmost = tuple(cnt[cnt[:,:,0].argmax()][0])
             topmost = tuple(cnt[cnt[:,:,1].argmin()][0])
             bottommost = tuple(cnt[cnt[:,:,1].argmax()][0])
+
+            bottomIsLeft = True
+            #check if bottommost is closest to right or left
+            if (abs(bottommost[0]-leftmost[0]) > abs(bottommost[0]-rightmost[0])):
+                print("bottom most is right")
+                bottomIsLeft = False
+
+            else:
+                 print("bottom most is left")
 
             # draw extreme points
             # from https://www.pyimagesearch.com/2016/04/11/finding-extreme-points-in-contours-with-opencv/
@@ -637,6 +746,121 @@ def findTape(contours, image, centerX, centerY):
             #print('extreme points', leftmost,rightmost,topmost,bottommost)
 
             #outer_corners = [leftmost, rightmost, topmost, bottommost]
+
+            upperleft_upperRight = np.array([leftmost, rightmost], dtype="double")
+            if (bottomIsLeft):
+                upperleft_bottomLeft = np.array([leftmost, bottommost], dtype="double")
+
+            else:
+                upperright_bottomRight = np.array([rightmost, bottommost], dtype="double")
+
+
+            rows,cols = image.shape[:2]
+            #[vx,vy,x,y] = cv.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01) #errors in VS Code, search online and found fix
+            #[vx,vy,x,y] = cv2.fitLine(cnt, cv2.DIST_L2,0,0.01,0.01)
+            [vx,vy,x,y] = cv2.fitLine(upperleft_upperRight, cv2.DIST_L2,0,0.01,0.01)
+            lefty = int((-x*vy/vx) + y)
+            righty = int(((cols-x)*vy/vx)+y)
+            cv2.line(image,(cols-1,righty),(0,lefty),(0,255,0),2)
+            print ("leftmost: " + str(leftmost))
+            print ("rightmost: " + str(rightmost))
+            print ("bottommost: " + str(bottommost))
+            print ("contour: " + str(cnt))
+
+            #find lines that go through top left and right corners
+            ls = 0
+            for i in range(len(cnt)):
+               if (np.all(cnt[i] == leftmost)):
+                   ls = i
+
+            #check number of points exists in array (safety check)       
+            cntlen = len(cnt)
+            #if difference between contour length and position of point is
+            #greater than 10, then use 10 points for line of best fit, otherwise use
+            #difference
+            numpoints = 15
+            if (cntlen-(ls+10) < 10):
+                numpoints = (cntlen-(ls+10))
+            print ("left start: " + str(ls)) 
+            left_edge_line = np.array([cnt[ls]])
+            for pts in range(numpoints):
+                left_edge_line = np.append(left_edge_line,np.array([cnt[ls+pts]]),axis=0)
+
+            print("left edge1: " + str(left_edge_line))
+            #left_edge_line = np.array([cnt[ls], cnt[ls+1], cnt[ls+2], cnt[ls+3], cnt[ls+4], cnt[ls+5], cnt[ls+6], cnt[ls+7]], dtype="double")
+            print("left edge2: " + str(left_edge_line))
+
+            [vx,vy,x,y] = cv2.fitLine(left_edge_line, cv2.DIST_L2,0,0.01,0.01)
+            lefty = int((-x*vy/vx) + y)
+            righty = int(((cols-x)*vy/vx)+y)
+            cv2.line(image,(cols-1,righty),(0,lefty),(0,255,0),2)
+
+            #find line for rightmost
+            ls = 0
+            for i in range(len(cnt)):
+               if (np.all(cnt[i] == rightmost)):
+                   ls = i
+                   
+
+            #if difference between contour length and position of point is
+            #greater than 10, then use 10 points for line of best fit, otherwise use
+            #difference
+            numpoints = 15
+            if ((ls-10) < 0):
+                numpoints = ls
+            print ("right start: " + str(ls)) 
+            right_edge_line = np.array([cnt[ls]])
+            for pts in range(numpoints):
+                right_edge_line = np.append(right_edge_line,np.array([cnt[ls-pts]]),axis=0)
+
+
+            print ("right start: " + str(ls)) 
+            #left_edge_line = np.array([cnt[ls], cnt[ls-1], cnt[ls-2], cnt[ls-3], cnt[ls-4]], dtype="double")
+
+            [vx,vy,x,y] = cv2.fitLine(right_edge_line, cv2.DIST_L2,0,0.01,0.01)
+            lefty = int((-x*vy/vx) + y)
+            righty = int(((cols-x)*vy/vx)+y)
+            cv2.line(image,(cols-1,righty),(0,lefty),(0,255,0),2)
+
+            #find bottom line
+            ls = 0
+            for i in range(len(cnt)):
+               if (np.all(cnt[i] == bottommost)):
+                   ls = i
+
+            if (bottomIsLeft):
+                numpoints = 8
+                if (cntlen-(ls+10) < 10):
+                    numpoints = (cntlen-(ls+10))
+                print ("bottom start: " + str(ls)) 
+                bottom_edge_line = np.array([cnt[ls]])
+                for pts in range(numpoints):
+                    bottom_edge_line = np.append(bottom_edge_line,np.array([cnt[ls+pts]]),axis=0)
+
+                print("bottom edge: " + str(bottom_edge_line))
+                #left_edge_line = np.array([cnt[ls], cnt[ls+1], cnt[ls+2], cnt[ls+3], cnt[ls+4], cnt[ls+5], cnt[ls+6], cnt[ls+7]], dtype="double")
+                #print("left edge2: " + str(left_edge_line))
+
+                [vx,vy,x,y] = cv2.fitLine(bottom_edge_line, cv2.DIST_L2,0,0.01,0.01)
+                lefty = int((-x*vy/vx) + y)
+                righty = int(((cols-x)*vy/vx)+y)
+                cv2.line(image,(cols-1,righty),(0,lefty),(0,255,0),2)
+                   
+
+
+            # http://ottonello.gitlab.io/selfdriving/nanodegree/python/line%20detection/2016/12/18/extrapolating_lines.html
+            #slope = vy / vx
+            #intercept = y - (slope * x)
+            #print('fitLine y = ', slope, '* x + ', intercept)
+            #cv2.fitLine()
+            #np.array(
+            #             [[H_FOCAL_LENGTH, 0, center[0]],
+            #             [0, V_FOCAL_LENGTH, center[1]],
+            #             [0, 0, 1]], dtype = "double"
+            #             )
+
+            imageWithCorners = findCorners(cnt,4)
+
 
             outer_corners = np.array([leftmost, rightmost, bottommost], dtype="double")
             sorted_corners = order_points(outer_corners)
@@ -650,9 +874,9 @@ def findTape(contours, image, centerX, centerY):
                 #result.extend(compute_output_values(rvec, tvec))
                 #return result
                 distance, angle1, angle2 = compute_output_values(rvec, tvec)
-                cv2.putText(image, "TargetYawToCenter: " + str(angle1), (40, 140), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
-                cv2.putText(image, "Distance: " + str(distance/12), (40, 190), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
-                cv2.putText(image, "RobotYawToTarget: " + str(angle2), (40, 240), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
+                cv2.putText(image, "TargetYawToCenter: " + str(angle1), (40, 340), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
+                cv2.putText(image, "Distance: " + str(distance/12), (40, 380), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
+                cv2.putText(image, "RobotYawToTarget: " + str(angle2), (40, 420), cv2.FONT_HERSHEY_COMPLEX, .6,(255, 255, 255))
                 #cv2.line(image, (finalTarget[0], screenHeight), (finalTarget[0], 0), (255, 0, 0), 2)
 
     #     currentAngleError = finalTarget[1]
@@ -1073,7 +1297,7 @@ while True:
     else:
 
         if Tape:
-
+            boxBlur = blurImg(frame, green_blur)
             threshold = threshold_video(lower_green, upper_green, frame)
             processed = findTargets(frame, threshold)
 
@@ -1108,8 +1332,3 @@ while True:
          currentImg = 0
 
     img = images[currentImg]
-
-
-
-
-
