c00f589c7fe8ddae8aac1424702aeabce6f3a879
jogray72 <34323688+jogray72@users.noreply.github.com>
2020-02-16 17:32:38 -0500

    Rename pipeline in build script
    buildScript.py now renames MergeFRCPipline.py to uploaded.py in our visionComp.zip file.

--- buildScript.py
+++ buildScript.py
@@ -14,5 +14,6 @@ zipObj.write('VisionConstants.py')
 zipObj.write('VisionMasking.py')
 zipObj.write('VisionUtilities.py')
 zipObj.write('NetworkTablePublisher.py')
+zipObj.write('MergeFRCPipeline.py','uploaded.py')
 
 print("I should have wrote the file: " + FILENAME)
