e46f47e81e6efecd417f70d9e8541ccb93155703
robertlucyshyn <robert_lucyshyn@yahoo.ca>
2020-02-22 15:26:20 -0500

    Update MergeViewer.py

--- MergeViewer.py
+++ MergeViewer.py
@@ -51,7 +51,7 @@ showAverageFPS = False
 
 # CHOOSE VIDEO OR FILES HERE!!!!
 # boolean for video input, if true does video, if false images
-useVideo = False
+useVideo = True
 # integer for usb camera to use, boolean for live webcam
 useWebCam = False
 webCamNumber = 1
@@ -125,7 +125,7 @@ ControlPanel = False
 # Method 9 is a five point visual method using SolvePNP (Brian and Erik)
 # Method 10 is a four point SolvePNP blending M6 and M7 (everybody!)
 
-Method = 4
+Method = 7
 
 if useVideo and not useWebCam:
     cap = cv2.VideoCapture(videoname)
