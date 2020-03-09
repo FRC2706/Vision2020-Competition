4ed70fb8892e0ca20a7fb841d4977e8ce1dbc765
jogray72 <34323688+jogray72@users.noreply.github.com>
2020-03-08 23:52:41 -0400

    Added exposure config setting
    Fixed bling
    Added exposure config setting
    Added output stream config setting
    updated build script to output 4 .zip files, pi20, pi21 for practise and competition bots.

--- PrintPublisher.py
+++ PrintPublisher.py
@@ -2,3 +2,6 @@
 
 def publishNumber(MergeVisionPipeLineTableName, name, value):
    print(MergeVisionPipeLineTableName + ": " + name + " : " + str(value))
+
+def publishString(TableName, name, value):
+   print(TableName + ": " + name + " : " + str(value))
